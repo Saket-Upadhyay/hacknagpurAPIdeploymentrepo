5dcad1e9436ce2a0c748834ebe4b7334eb50aa38
Saket Upadhyay <saketupadhya@gmail.com>
2020-12-31 21:40:06 +0530

    space issue solve

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask
 from flask import request
+from flask import session
+import random
 import json
 from flask_cors import CORS, cross_origin
-
 
 import modules.website_analysis as wa
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+app.secret_key = "".join(random.sample("a b c d e f  g h i j k l m n o p q r s t u v w x y z".split(), 20))
 
 
+# print(app.secret_key)
 def calc_score(data):
     page_achievement = data['pages']
     page_achievement = page_achievement[0]
@@ -36,6 +39,7 @@ def calc_score(data):
 @app.route('/')
 @cross_origin()
 def home():
+    session.clear()
     try:
         # print("initiating scan")
         # st = "https://saket-upadhyay.github.io/"
@@ -52,9 +56,9 @@ def home():
         #
         # report.update(result)
 
-        report= "Chintu SEO Services. Home Page. send a POST/GET request to chintu on | /scan?site=[http(s)://yousitename.domain/]" \
-                "<br><br><br><br><br><br> (This name is result of internal joke [because our team name is WhiteHatJuniors, fun spinoff of recent events in India :) ], we have nothing to do with Chintu, we don't even know any.)" \
-                "<br><br><br><br><br><br> The actual project name is <strong>WebFlux</strong>"
+        report = "Chintu SEO Services. Home Page. send a POST/GET request to chintu on | /scan?site=[http(s)://yousitename.domain/]" \
+                 "<br><br><br><br><br><br> (This name is result of internal joke [because our team name is WhiteHatJuniors, fun spinoff of recent events in India :) ], we have nothing to do with Chintu, we don't even know any.)" \
+                 "<br><br><br><br><br><br> The actual project name is <strong>WebFlux</strong>"
 
         return report
     except:
@@ -66,9 +70,18 @@ def home():
 def scan():
     if request.method == "POST":
         try:
+            session.clear()
             site = request.form.get('site')
+
+            while site[-1] == ' ':
+                temp = ""
+                for i in range(0, len(site) - 1):
+                    temp = temp + site[i]
+                site = temp
+
             if site[-1] != '/':
-                site=site+"/"
+                site = site + "/"
+
             print("Scanning " + str(site))
             Spider_Object = wa.Spider(site)
             print("Initiating Crawl")
@@ -88,33 +101,41 @@ def scan():
         except:
             return "Internal Error in processing"
     elif request.method == "GET":
-        try:
-            site = request.args.get('site')
-            if site[-1] != '/':
-                site=site+"/"
-            print("Scanning " + str(site))
-            Spider_Object = wa.Spider(site)
-            print("Initiating Crawl")
-            raw_report = Spider_Object.crawl()
-            print("Crawl Over")
-            print("Sending response")
-            report = json.loads(json.dumps(raw_report, indent=4, separators=(',', ': ')))
-            data = report
 
-            site_achievement, page_achievement, site_issues, page_issues, total_achievement, total_issues, max_score = calc_score(
-                data)
-            result = {'scored': total_achievement, 'max_score': max_score}
+        session.clear()
+        site = request.args.get('site')
 
-            report.update(result)
+        while site[-1] == ' ':
+            temp = ""
+            for i in range(0, len(site) - 1):
+                temp = temp + site[i]
 
-            return report
-        except:
-            return "Internal Error in processing"
+            site = temp
+
+        if site[-1] != '/':
+            site = site + "/"
+
+        print("Scanning " + str(site))
+        Spider_Object = wa.Spider(site)
+        print("Initiating Crawl")
+        raw_report = Spider_Object.crawl()
+        print("Crawl Over")
+        print("Sending response")
+        report = json.loads(json.dumps(raw_report, indent=4, separators=(',', ': ')))
+        data = report
+
+        site_achievement, page_achievement, site_issues, page_issues, total_achievement, total_issues, max_score = calc_score(
+            data)
+        result = {'scored': total_achievement, 'max_score': max_score}
+
+        report.update(result)
+
+        return report
+
+        return "Internal Error in processing"
     else:
         return "ERROR: API accepts POST or GET requests only."
 
 
 if __name__ == '__main__':
     app.run(threaded=True)
-    
-    
